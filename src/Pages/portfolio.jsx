a37d770ae08791423abf58c21d9d6dd840378fa0
Your Name <your-email@example.com>
2025-01-03 02:20:59 +0600

    chabge

--- src/Pages/portfolio.jsx
+++ src/Pages/portfolio.jsx
@@ -2,61 +2,78 @@ import React from "react";
 
 const Portfolio = () => {
   return (
-    <div className="bg-[#292929] p-16">
-      <div className=" flex justify-center items-center ">
-        <p className="text-white text-6xl font-semibold ">
+    <div className="bg-gray-800 p-16">
+      <div className="flex justify-center items-center">
+        <p className="text-white text-6xl font-semibold">
           My <span>Portfolio</span>
         </p>
       </div>
 
-      <div className="m-16  relative  ">
-        <div className="h-[200px] w-[350px] absolute top-0 left-0  ">
+      <div className="m-16 relative h-screen">
+        {/* Top row */}
+        <div className="absolute top-0 left-0 h-48 w-80">
           <img
             src="https://slimhamdi.net/sliim/demos/images/projects/4.jpg"
-            alt=""
+            alt="Portfolio item 1"
+            className="w-full h-full object-cover"
           />
         </div>
 
-        <div className="h-[400px] w-[300px] absolute top-0 left-[400px]">
+        <div className="absolute top-0 left-96 h-96 w-72">
           <img
             src="https://slimhamdi.net/sliim/demos/images/projects/1.jpg"
-            alt=""
+            alt="Portfolio item 2"
+            className="w-full h-full object-cover"
           />
         </div>
-        <div className="h-[400px] w-[300px] absolute top-0 left-[760px]">
+
+        <div className="absolute top-0 right-0 h-96 w-72">
           <img
             src="https://slimhamdi.net/sliim/demos/images/projects/3.jpg"
-            alt=""
+            alt="Portfolio item 3"
+            className="w-full h-full object-cover"
           />
         </div>
-        <div className="h-[450px] w-[300px] absolute top-[270px]">
+
+        {/* Middle row */}
+        <div className="absolute top-64 left-0 h-96 w-72">
           <img
             src="https://slimhamdi.net/sliim/demos/images/projects/8.jpg"
-            alt=""
+            alt="Portfolio item 4"
+            className="w-full h-full object-cover"
           />
         </div>
-        <div className="h-[200px] w-[300px] absolute top-[440px] left-[400px]">
+
+        <div className="absolute top-96 left-96 h-48 w-72">
           <img
             src="https://slimhamdi.net/sliim/demos/images/projects/5.jpg"
-            alt=""
+            alt="Portfolio item 5"
+            className="w-full h-full object-cover"
           />
         </div>
-        <div className="h-[400px] w-[300px] absolute top-[490px] left-[760px]">
+
+        <div className="absolute top-96 right-0 h-96 w-72">
           <img
             src="https://slimhamdi.net/sliim/demos/images/projects/2.jpg"
-            alt=""
+            alt="Portfolio item 6"
+            className="w-full h-full object-cover"
           />
         </div>
-        <div className="h-[200px] w-[350px] absolute top-[760px]">
+
+        {/* Bottom row */}
+        <div className="absolute bottom-0 left-0 h-48 w-80">
           <img
             src="https://slimhamdi.net/sliim/demos/images/projects/6.jpg"
-            alt=""
+            alt="Portfolio item 7"
+            className="w-full h-full object-cover"
           />
         </div>
-        <div className="h-[400px] w-[300px] absolute top-[680px] left-[400px]">
+
+        <div className="absolute bottom-0 left-96 h-96 w-72">
           <img
             src="https://slimhamdi.net/sliim/demos/images/projects/7.jpg"
-            alt=""
+            alt="Portfolio item 8"
+            className="w-full h-full object-cover"
           />
         </div>
       </div>
