24a0aba2201d4a3d4d7afa90e11ad20bcbcf51f0
Your Name <your-email@example.com>
2025-01-04 00:45:34 +0600

    route

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,29 @@ import AboutMe from "./Pages/About/AboutMe";
 import Portfolio from "./Pages/portfolio";
 import Contract from "./Pages/contract";
 import Blog from "./Pages/blog";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <Home />
-        <AboutMe />
-        <Portfolio />
-        <Contract />
-        <Blog />
-      </div>
+      <BrowserRouter>
+        <div>
+          <Home />
+          <AboutMe />
+          <Portfolio />
+          <Contract />
+          <Blog />
+        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutMe />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contract" element={<Contract />} />
+          <Route path="/blog" element={<Blog />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
